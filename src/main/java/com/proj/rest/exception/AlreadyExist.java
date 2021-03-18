429842df0a920e0c9ac5e0dc6c0a1273fa2c070e
faith <faith@LAPTOP-TJ615OK7>
2021-03-18 17:01:59 +0530

    Coomit 2 after adding logger information

--- src/main/java/com/proj/rest/exception/AlreadyExist.java
+++ src/main/java/com/proj/rest/exception/AlreadyExist.java
@@ -1,10 +1,13 @@
 package com.proj.rest.exception;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class AlreadyExist extends RuntimeException {
+	private static Logger LOGGER = LogManager.getLogger(AlreadyExist.class);
 
 	/**
 	 * 
@@ -12,6 +15,8 @@ public class AlreadyExist extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	public AlreadyExist(String message) {
 		super(message);
+		LOGGER.debug(">>>>>>>>>>Cotrol comes in AlreadyExist constructor<<<<<<<<<<");
+		
 	}
 	
 
